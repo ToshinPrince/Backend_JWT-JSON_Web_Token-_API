5340a96b1374a9eacd61ab3815063eae88de7423
Toshin Prince <toshinprince@gmail.com>
2023-11-27 17:43:49 +0530

    setting up routes controllers

---
+++ controllers/main.js
@@ -0,0 +1,13 @@
+const login = async (req, res) => {
+  res.send("Fake Login/Register/Signup Route");
+};
+
+const dashboard = async (req, res) => {
+  const luckeyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `hello, joy pollo`,
+    secret: `Here is your authorised data, here is your luckey Number ${luckeyNumber}`,
+  });
+};
+
+module.exports = { login, dashboard };
