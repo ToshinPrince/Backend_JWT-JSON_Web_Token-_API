65533f8db34e21b8210220945c35895e73e54ccd
Toshin Prince <toshinprince@gmail.com>
2023-11-28 17:59:03 +0530

    custom error to login controller

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,19 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  //ways to check if username and password is provided or not to give error message
+  //mongodb - or and database will give error by default
+  //joi - web pack
+  //check in the controller - This is what we are doing below, because initailly we are not connecting to the data base, will do in future update
+  if ((!username || !password)) {
+    throw new CustomAPIError(
+      "Please Provide Custom username and password",
+      400
+    );
+  }
+
   res.send("Fake Login/Register/Signup Route");
 };
 
